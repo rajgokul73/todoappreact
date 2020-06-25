3ee2a22f618edfc8cf8508aea103a964156f0b95
gokulraj <mgokulraj73@gmail.com>
2020-06-25 13:19:12 +0530

    Updates

---
+++ precache-manifest.7e6ef613e6e8f88dc6cc7bea28d7954e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac58030d13e69c61a886282bd305390f",
+    "url": "/todoappreact/index.html"
+  },
+  {
+    "revision": "09a4857720808301a898",
+    "url": "/todoappreact/static/css/main.9bfb70d7.chunk.css"
+  },
+  {
+    "revision": "bd0c97b7a831b9122b2d",
+    "url": "/todoappreact/static/js/2.0512a152.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoappreact/static/js/2.0512a152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09a4857720808301a898",
+    "url": "/todoappreact/static/js/main.6b203219.chunk.js"
+  },
+  {
+    "revision": "18f2240d43191783d34d",
+    "url": "/todoappreact/static/js/runtime-main.a1396971.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoappreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
